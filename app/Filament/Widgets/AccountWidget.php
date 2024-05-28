e584762507eb688494c508d71726865c51dfc82d
wirandra.rembon <alayarahman07@gmail.com>
2024-05-28 16:58:18 +0700

    adj: set width of account widget to column span full

---
+++ app/Filament/Widgets/AccountWidget.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Filament\Widgets;
+
+use Filament\Widgets\AccountWidget as WidgetsAccountWidget;
+
+class AccountWidget extends WidgetsAccountWidget
+{
+    protected int | string | array $columnSpan = 'full';
+}
